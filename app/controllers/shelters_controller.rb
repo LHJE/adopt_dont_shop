96b3830edc277f5d5a6b163a1af8ae76847c5941
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-19 15:48:19 -0600

    Shelter adding form created

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -2,4 +2,10 @@ class SheltersController < ApplicationController
   def index
     @shelters = ['Dumb Friends League', 'Denver Samoyed Rescue']
   end
+
+  def new
+  end
+
+  def create
+  end
 end
