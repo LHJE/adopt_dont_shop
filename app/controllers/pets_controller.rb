d51e210c7fe1d0ea7a4676fa5c1eff9841f2b0dd
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-19 20:48:56 -0600

    Pet page text and starting attributes established

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,48 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def new
+  end
+
+  def create
+    pet = Pet.new({
+      image: params[:pet][:image],
+      name: params[:pet][:name],
+      age: params[:pet][:age],
+      sex: params[:pet][:sex],
+      shelter_id: params[:pet][:shelter_id]
+      })
+
+    pet.save
+
+    redirect_to '/pets'
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update({
+      image: params[:pet][:image],
+      name: params[:pet][:name],
+      age: params[:pet][:age],
+      sex: params[:pet][:sex],
+      shelter_id: params[:pet][:shelter_id]
+      })
+    pet.save
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  def destroy
+    Pet.destroy(params[:id])
+    redirect_to '/pets'
+  end
+end
---
+++ spec/features/pets/index_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "pets index page" do
+  before :each do
+    @pet_1 = Pet.create(  image:      "image of some sort",
+                          name:       "Eureka",
+                          age:        "3",
+                          sex:        "M",
+                          shelter_id: 1
+                        )
+    @pet_2 = Pet.create(  image:      "image of some sort",
+                          name:       "Kadfly",
+                          age:        "5",
+                          sex:        "F",
+                          shelter_id: 2
+                        )
+  end
+
+  it "can see all pets attributes" do
+    visit "/pets"
+  expect(page).to have_content(@pet_1.image)
+  expect(page).to have_content(@pet_1.name)
+  expect(page).to have_content(@pet_1.age)
+  expect(page).to have_content(@pet_1.sex)
+  expect(page).to have_content(@pet_1.shelter_id)
+  expect(page).to have_content(@pet_2.image)
+  expect(page).to have_content(@pet_2.name)
+  expect(page).to have_content(@pet_2.age)
+  expect(page).to have_content(@pet_2.sex)
+  expect(page).to have_content(@pet_2.shelter_id)
+  end
+end
