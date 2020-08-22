f7b947e595bb7a569b88a9e3c10b6edaa7b12eae
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-21 23:34:23 -0600

    still struggling with the photo upload

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,16 +9,18 @@ class PetsController < ApplicationController
 
   def create
     @pet = Pet.new({
+      image: params[:pet][:image],
       name: params[:pet][:name],
       age: params[:pet][:age],
       sex: params[:pet][:sex],
       shelter_id: params[:pet][:shelter_id]
       })
-    pet.save
+    binding.pry
+    @pet.save
     if @pet.save
-      flash[:notice] = "Successfully added new photo!"
+      flash[:notice] = "Successfully added new pet!"
     else
-      flash[:alert] = "Error adding new photo!"
+      flash[:alert] = "Error adding new pet!"
       render :new
     end
   end
@@ -32,7 +34,7 @@ class PetsController < ApplicationController
   end
 
   def update
-    pet = Pet.find(params[:id])
+    @pet = Pet.find(params[:id])
     pet.update({
       image: params[:pet][:image],
       name: params[:pet][:name],
@@ -40,12 +42,19 @@ class PetsController < ApplicationController
       sex: params[:pet][:sex],
       shelter_id: params[:pet][:shelter_id]
       })
-    pet.save
-    redirect_to "/pets/#{pet.id}"
+    @pet.save
+    redirect_to "/pets/#{@pet.id}"
   end
 
   def destroy
     Pet.destroy(params[:id])
     redirect_to '/pets'
+  end
+
+  private
+
+  #Permitted parameters when creating a photo. This is used for security reasons.
+  def photo_params
+    params.require(:photo).permit(:title, :image)
   end
 end
