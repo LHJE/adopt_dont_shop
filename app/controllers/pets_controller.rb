0e422fcfe212257d7d3e3e623608372e357c4272
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-21 23:17:57 -0600

    editing new pet to add picture instead of placeholder text

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,17 +9,14 @@ class PetsController < ApplicationController
 
   def create
     @pet = Pet.new({
-      image: params[:pet][:image],
       name: params[:pet][:name],
       age: params[:pet][:age],
       sex: params[:pet][:sex],
       shelter_id: params[:pet][:shelter_id]
       })
     pet.save
-    binding.pry
     if @pet.save
       flash[:notice] = "Successfully added new photo!"
-      redirect_to '/pets'
     else
       flash[:alert] = "Error adding new photo!"
       render :new
