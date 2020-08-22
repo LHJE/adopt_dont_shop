b921f76c58f0be00bdae6d318eadf5c3196e7b39
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-22 13:22:08 -0600

    Commiting to try to do Active Storage third time.

---
+++ app/views/pets/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "pets/image", image: @pet
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: "images#index"
   resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
